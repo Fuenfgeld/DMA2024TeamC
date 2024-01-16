2c91de6d3e589001d3b8c54dea632d0cfe266697
Mónica Espitia <39849786+moanesga@users.noreply.github.com>
2024-01-16 15:59:06 +0100

    Update create_statements_db.sql
    Patients table

--- create_statements_db.sql
+++ create_statements_db.sql
@@ -1,1 +1,30 @@
 
+CREATE TABLE IF NOT EXISTS patients (
+                           Id         STRING PRIMARY KEY,
+                           BIRTHDATE  DATE,
+                           DEATHDATE  DATE,
+                           SSN        STRING,
+                           DRIVERS    STRING,
+                           PASSPORT   STRING,
+                           PREFIX     STRING,
+                           FIRST      STRING,
+                           LAST       STRING,
+                           SUFFIX     STRING,
+                           MAIDEN     STRING,
+                           MARITAL    STRING,
+                           RACE       STRING,
+                           ETHNICITY  STRING,
+                           GENDER     STRING,
+                           BIRTHPLACE STRING,
+                           ADDRESS    STRING,
+                           CITY       STRING,
+                           STATE      STRING,
+                           COUNTRY    STRING,
+                           ZIP        STRING,
+                           LAT        INTEGER,
+                           LON        INTEGER,
+                           HEALTHCARE_EXPENSES INTEGER,
+                           HEALTHCARE_COVERAGE INTEGER
+  );
+                       
+
