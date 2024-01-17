5f6c2943fefc6c09d3287dd995879251f1f5fd39
Mónica Espitia <39849786+moanesga@users.noreply.github.com>
2024-01-17 16:55:40 +0100

    Update create_statements_db.sql
    Added other tables

--- Code/create_statements_db.sql
+++ Code/create_statements_db.sql
@@ -27,4 +27,120 @@ CREATE TABLE IF NOT EXISTS patients (
                            HEALTHCARE_COVERAGE INTEGER
   );
                        
+CREATE TABLE IF NOT EXISTS conditions (
+                           condition_code INTEGER PRIMARY KEY AUTOINCREMENT,
+                           START DATE,
+                           STOP DATE,
+                           PATIENT STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+                           FOREIGN KEY (Encounter)
+                              REFERENCES encounters (Id) 
+ );
+
+
+CREATE TABLE IF NOT EXISTS observations (
+                           observation_code INTEGER PRIMARY KEY AUTOINCREMENT,
+                           DATE DATE,
+                           PATIENT STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           VALUE STRING,
+                           UNITS STRING,
+                           TYPE STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+                           FOREIGN KEY (Encounter)
+                              REFERENCES encounters (Id) 
+);
+
+CREATE TABLE IF NOT EXISTS careplans (
+                           Id STRING PRIMARY KEY,
+                           START DATE,
+                           STOP DATE,
+                           PATIENT STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           REASONCODE STRING,
+                           REASONDESCRIPTION STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+                           FOREIGN KEY (Encounter)
+                              REFERENCES encounters (Id) 
+                       );
+
+CREATE TABLE IF NOT EXISTS medications (
+                           START DATE,
+                           STOP DATE,
+                           PATIENT STRING,
+                           PAYER STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           BASE_COST INTEGER,
+                           PAYER_COVERAGE INTEGER,
+                           DISPENSES INTEGER,
+                           TOTALCOST INTEGER,
+                           REASONCODE STRING,
+                           REASONDESCRIPTION STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+  );
+
+CREATE TABLE IF NOT EXISTS procedures (
+                           DATE DATE,
+                           PATIENT STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           BASE_COST INTEGER,
+                           REASONCODE STRING,
+                           REASONDESCRIPTION STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+                           FOREIGN KEY (Encounter)
+                              REFERENCES encounters (Id) 
+                       );
+ 
+CREATE TABLE IF NOT EXISTS immunizations(
+                           DATE DATE,
+                           PATIENT STRING,
+                           ENCOUNTER STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           BASE_COST INTEGER,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+                           FOREIGN KEY (Encounter)
+                              REFERENCES encounters (Id) 
+  );
+
+CREATE TABLE IF NOT EXISTS encounters (
+                           Id STRING PRIMARY KEY,
+                           START DATE,
+                           STOP DATE,
+                           PATIENT STRING,
+                           ORGANIZATIONS STRING,
+                           PROVIDER STRING,
+                           PAYER STRING,
+                           ENCOUNTERCLASS STRING,
+                           CODE STRING,
+                           DESCRIPTION STRING,
+                           BASE_ENCOUNTER_COST INTEGER,
+                           TOTAL_CLAIM_COST INTEGER,
+                           PAYER_COVERAGE INTEGER,
+                           REASONCODE STRING,
+                           REASONDESCRIPTION STRING,
+                           FOREIGN KEY (PATIENT)
+                              REFERENCES patients (Id) 
+);
+
+
+
+
 
